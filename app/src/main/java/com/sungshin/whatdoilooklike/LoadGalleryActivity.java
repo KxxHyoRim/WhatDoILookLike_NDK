e5baaec3bd4e39795ccdab58069965dbf2dc9560
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2021-08-17 21:58:01 +0900

    add gallert button

--- app/src/main/java/com/sungshin/whatdoilooklike/LoadGalleryActivity.java
+++ app/src/main/java/com/sungshin/whatdoilooklike/LoadGalleryActivity.java
@@ -1,5 +1,6 @@
 package com.sungshin.whatdoilooklike;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -8,6 +9,8 @@ import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,6 +28,9 @@ import java.io.InputStream;
 public class LoadGalleryActivity extends AppCompatActivity {
 
     File tempFile;
+    ImageView imageview;
+    Button button;
+    Mat mat;
 
     static {
         System.loadLibrary("opencv_java4");
@@ -36,7 +42,8 @@ public class LoadGalleryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
-        ImageView imageview = (ImageView)findViewById(R.id.imageView);
+        imageview = (ImageView)findViewById(R.id.imageView);
+        button = (Button) findViewById(R.id.button);
 
         Uri uri = Uri.parse(getIntent().getStringExtra("bitmap"));
         Uri mPhotoUri = Uri.parse(getRealPathFromURI(uri));
@@ -60,10 +67,56 @@ public class LoadGalleryActivity extends AppCompatActivity {
         bitmap = rotateBitmap(bitmap, orientation);
 
         //갤러리 이미지 Mat
-        Mat mat = new Mat();
+        mat = new Mat();
         Utils.bitmapToMat(bitmap, mat);
 
         imageview.setImageBitmap(bitmap);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 갤러리로 이동
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                Uri uri = data.getData();
+                Uri mPhotoUri = Uri.parse(getRealPathFromURI(uri));
+
+                ExifInterface exif = null;
+                try {
+                    exif = new ExifInterface(mPhotoUri.getPath());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                        ExifInterface.ORIENTATION_UNDEFINED);
+
+                InputStream in = null;
+                try {
+                    in = getContentResolver().openInputStream(uri);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                Bitmap bitmap = BitmapFactory.decodeStream(in);
+                bitmap = rotateBitmap(bitmap, orientation);
+
+                //갤러리 이미지 Mat
+                Utils.bitmapToMat(bitmap, mat);
+
+                imageview.setImageBitmap(bitmap);
+            }
+        }
     }
 
     private String getRealPathFromURI(Uri contentURI) {
